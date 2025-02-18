1305cd025428946699c4043875d4ebb9f0640b4f
Roberto Sampaio <ssampaio.roberto@gmail.com>
2025-02-18 17:57:30 -0300

    Initial Commit

---
+++ Checkers/CheckersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckersApp.swift
+//  Checkers
+//
+//  Created by Betow on 18/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CheckersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckersTests/CheckersTests.swift
@@ -0,0 +1,17 @@
+//
+//  CheckersTests.swift
+//  CheckersTests
+//
+//  Created by Betow on 18/02/25.
+//
+
+import Testing
+@testable import Checkers
+
+struct CheckersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
